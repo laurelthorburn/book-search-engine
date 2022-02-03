a98bd4b084ab62f26d3d98c4ce4db04924420ce3
Laurel Thorburn <laurelthorburn@gmail.com>
2022-02-03 13:22:55 -0700

    Added query with me

---
+++ client/src/utils/queries.js
@@ -0,0 +1,15 @@
+import { gql } from '@apollo/client';
+
+export const QUERY_ME = gql`
+  query me {
+    me {
+      _id
+      username
+      email
+      bookCount
+      savedBooks
+    }
+  }
+`;
+
+
