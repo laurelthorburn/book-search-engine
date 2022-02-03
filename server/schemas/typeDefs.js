58d05bcc42ff58dfdeff6e2a0e2f472d84b11012
Laurel Thorburn <laurelthorburn@gmail.com>
2022-02-03 11:53:21 -0700

    editing typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -4,11 +4,11 @@ const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
   type User {
-    _id: ID
-    username: String
-    email: String
-    bookCount: Number
-    savedBooks: [Book]!
+    _id: ID!
+    username: String!
+    email: String!
+    bookCount: Int
+    savedBooks: [Book]
   }
 
   type Book {
--- server/server.js
+++ server/server.js
@@ -16,8 +16,14 @@ const server = new ApolloServer({
   resolvers,
 });
 
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true })); //true or false?
 app.use(express.json());
+
+// if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
@@ -28,6 +34,7 @@ app.get('*', (req, res) => {
 db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
+    //playground for GQL API
     console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
   });
 });
