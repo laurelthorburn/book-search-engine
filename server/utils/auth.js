4365e5bb1ab12f7e6d15773cd11a4a32532a9698
Laurel Thorburn <laurelthorburn@gmail.com>
2022-02-03 19:27:32 -0700

    Switch authupdated autho middleware function to work with graphql API

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -4,20 +4,10 @@ const jwt = require('jsonwebtoken');
 const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
-
-
-// module.exports = {
-//   signToken: function ({ email, username, _id }) {
-//     const payload = { email, username, _id };
-//     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
-//   },
-// };
-
 module.exports = {
-  // function for our authenticated routes
-  authMiddleware: function (req, res, next) {
-    // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
+  // creating auth routes, trying to convert
+  authMiddleware: function ({req}) {
+    let token = req.body.token || req.query.token || req.headers.authorization;
 
     // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
@@ -38,7 +28,7 @@ module.exports = {
     }
 
     // send to next endpoint
-    next();
+    return req;
   },
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
