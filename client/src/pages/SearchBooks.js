5dd20257137c3e2aa6435ecc781cd8bf319e23e2
Laurel Thorburn <laurelthorburn@gmail.com>
2022-02-03 17:41:48 -0700

    Added query import to searchBooks

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
+import { QUERY_MATCHUPS } from '../utils/queries';
 
 import Auth from '../utils/auth';
-import { saveBook, searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
 
 const SearchBooks = () => {
