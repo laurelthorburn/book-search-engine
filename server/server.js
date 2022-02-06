66cf3a0ef99141dda45663951f9c21e45d277970
Laurel Thorburn <laurelthorburn@gmail.com>
2022-02-06 09:17:07 -0700

    OH MY LANTA, found the mega bug.  had authMiddle ware pulling from utils/autho instead of schema, causing playground to get 500 server!!

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const path = require('path');
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 
-const { authMiddleware } = require('./utils/auth');
+const { authMiddleware } = require('./schemas');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -21,7 +21,7 @@ const server = new ApolloServer({
 
 server.applyMiddleware({ app });
 
-app.use(express.urlencoded({ extended: true })); //true or false?
+app.use(express.urlencoded({ extended: false })); //true or false?
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
